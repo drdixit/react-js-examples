bc41b061ac3d450d41a7179669e76ba3a69583de
Dixit <drdixit6@gmail.com>
2025-07-22 16:28:02 +0530

    jsx introduction

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-const pizza = (props) => {
-    return React.createElement(
-        "div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("p", {}, props.description)
-    ])
-}
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
+
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "React JS with cdn"),
-            React.createElement(pizza, {
-                name: "Pepperoni",
-                description: "A classic pizza with pepperoni slices"
-            }),
-            React.createElement(pizza, {
-                name: "Alfredo",
-                description: "A creamy white sauce pizza with chicken and spinach"
-            }),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "React JS with cdn"),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "A classic pizza with pepperoni slices",
+    }),
+    React.createElement(Pizza, {
+      name: "Alfredo",
+      description: "A creamy white sauce pizza with chicken and spinach",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
